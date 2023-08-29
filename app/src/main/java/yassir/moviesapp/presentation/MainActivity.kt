704f0aa2223410699c50eb498399d73a7fb2de2a
Abdel Rahman Shaltoot <arakmmis@gmail.com>
2023-08-29 11:03:20 +0300

    [Add] Splash fragment and main activity logic

--- app/src/main/java/yassir/moviesapp/presentation/MainActivity.kt
+++ app/src/main/java/yassir/moviesapp/presentation/MainActivity.kt
@@ -1,7 +1,18 @@
 package yassir.moviesapp.presentation
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import yassir.moviesapp.databinding.ActMainBinding
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/yassir/moviesapp/presentation/splash/SplashFragment.kt
@@ -0,0 +1,14 @@
+package yassir.moviesapp.presentation.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import yassir.moviesapp.R
+
+@AndroidEntryPoint
+class SplashFragment : Fragment(R.layout.frag_splash) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
